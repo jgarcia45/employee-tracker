2810d2cd2daaa1431e6b597dd809c53da9288ba4
Juan <jgarcia1545@gmail.com>
2022-04-29 19:27:29 -0700

    Contains access to my database

--- db/connection.js
+++ db/connection.js
@@ -1,12 +1,13 @@
 const mysql = require('mysql2');
+//require('dotenv').config();
 
 const db = mysql.createConnection({
     host: 'localhost',
     // Your MySQL username,
     user: 'root',
     // Your MySQL password
-    password: '',
-    database: 'election'
+    password: '60DGmO7!54rgXtR',
+    database: 'employee_tracker_db'
 });
 
 module.exports = db;
